147b26659bdce0df189573efe89b2939edcaaa44
jason51102000 <jason51102000@gmail.com>
2021-12-27 19:12:11 +0800

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
+  },
+  trailingSlash: false,
+  assetPrefix: "./",
   exportPathMap: async function(
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
