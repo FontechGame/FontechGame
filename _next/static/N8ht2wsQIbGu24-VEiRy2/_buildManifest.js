05c8cc14a941d605a533e40e6224f383e18da086
FontechGame <96712993+FontechGame@users.noreply.github.com>
2021-12-28 05:53:16 +0000

    Deploying to gh-pages from @ FontechGame/FontechGame@2672520961317233bfd74a9b81486f5159760a15 🚀

--- _next/static/N8ht2wsQIbGu24-VEiRy2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4e17398419f79ea3.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/tetris":["static/chunks/pages/tetris-a9846eb28d2b662c.js"],sortedPages:["/","/_app","/_error","/tetris"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
